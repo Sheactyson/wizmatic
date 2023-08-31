e8ea75c1f81cedf06d3ebada5f66a0bc8c6a37fe
Sheactyson <sheactyson@gmail.com>
2023-08-31 19:31:28 -0400

    Created a class to include all the values I will be pulling from the wiki Completed the majority of web scraping for the wiki parser in terms of cards 	new file:   scripts/__pycache__/resources.cpython-311.pyc 	modified:   scripts/parseCard.py 	modified:   scripts/resources.py

--- scripts/parseCard.py
+++ scripts/parseCard.py
@@ -1,6 +1,7 @@
 import requests
 import resources as rc
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 def importPage(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
@@ -9,48 +10,88 @@ def importPage(url):
     return soup
 
 def decodePage(inputName):
+    res = rc.LibCard()
+
     #Determine buffs or not
     if '^' in inputName:
-        originalName = inputName.split('^')[0]
-        buffName = inputName.split('^')[1]
+        res.originalName = inputName.split('^')[0]
+        res.buffName = inputName.split('^')[1]
     else:
-        originalName = inputName
-        buffName = ''
+        res.originalName = inputName
+        res.buffName = ''
 
     #Get page HTML
-    spellURL = rc.WIKIURL + '/wiki/Spell:' + originalName
-    cardHTML = importPage(spellURL)
+    res.spellURL = rc.WIKIURL + '/wiki/Spell:' + res.originalName
+    cardHTML = importPage(res.spellURL)
 
     #Get card name
-    lib_cardName = str(cardHTML.find(id='firstHeading')).split('Spell:')[1].split('<')[0]
+    res.cardName = str(cardHTML.find(id='firstHeading')).split('Spell:')[1].split('<')[0]
 
     if '^' in inputName:
-        lib_cardName = lib_cardName + '^' + buffName
+        res.cardName = res.cardName + '^' + res.buffName
 
     #iterate through images
     iURL_found = False
     school_found = False
+    shadowPip_found = False
     allImages = cardHTML.find_all('a', class_='image')
     for ele in allImages:
-        print(ele)
         #Get url of image on wiki
-        if(iURL_found is False and originalName in str(ele) and buffName in str(ele)):
-            lib_imgUrl = rc.WIKIURL + str(ele).split('src=\"')[1].split('\"')[0]
+        if(iURL_found is False and res.originalName in str(ele) and res.buffName in str(ele)):
+            res.imgURL = rc.WIKIURL + str(ele).split('src=\"')[1].split('\"')[0]
             iURL_found  = True
         #Get school of card
-        if(school_found is False and any(sch in str(ele) for sch in rc.SCHOOLS)):
-            lib_school = str(ele).split('(Icon) ')[1].split('.')[0]
+        elif(school_found is False and any(sch in str(ele) for sch in rc.SCHOOLS)):
+            res.school = str(ele).split('(Icon) ')[1].split('.')[0]
             school_found = True
         #Get pip cost
-        if('Pip' in str(ele)):
-            if any(sch in str(ele) for sch in rc.SCHOOLS):
-                lib_schoolPipCost = str(ele.parent.text).strip()
+        elif('Pip' in str(ele)):
+            if('Shadow' in str(ele)):
+                res.shadowPipCost = str(ele.parent.text).strip()
+                shadowPip_found = True
+            elif(shadowPip_found is False and any(sch in str(ele) for sch in rc.SCHOOLS)):
+                res.schoolPipCost = str(ele.parent.text).strip()
             else:
-                lib_regularPipCost = str(ele.parent.text).strip()
-                lib_schoolPipCost = 0
+                regularPipCost = str(ele.parent.text).strip()
+            
+            if(regularPipCost == "X"): #Account for all pips spells
+                res.regularPipCost = -1
+            else:
+                res.regularPipCost = regularPipCost
+        #Get type
+        elif(any(icn in str(ele) for icn in rc.SPELL_TYPE_ICONS)):
+            res.type.append(str(ele).split('title=\"')[1].split('\"')[0])
 
-    results = lib_cardName+'\n'+lib_imgUrl+'\n'+lib_school+'\n'+lib_regularPipCost+'+'+lib_schoolPipCost
-    print(results)
+    #Get base accuracy
+    accuracy_found = False
+    containers = cardHTML.find_all('td')
+    for cont in containers:
+        if(accuracy_found is False and '%' in str(cont.string)):
+            res.baseAccuracy = int(str(cont.string).replace('%',''))
+            accuracy_found = True
 
-cardName = 'Ship_of_Fools'
-decodePage(cardName)
+    #Grab description
+    desc_found = False
+    containers = cardHTML.find_all('td', {'colspan': '2'})
+    for ele in containers:
+        if('Description' in str(ele)):
+            desc_found = True
+            continue
+        elif(desc_found):         
+            res.desc = str(ele).split('>')[1].split('<')[0]
+            break
+
+    #Populate card values based on type
+    #for()
+
+    return res
+
+def printCardStatus(cardName):
+    card = decodePage(cardName)
+    card_vars = vars(card)
+    pprint(card_vars, sort_dicts=False)
+    print('\n')
+
+printCardStatus('Ship_of_Fools^Epic')
+printCardStatus('Call_of_Khrulhu')
+printCardStatus('Ship_of_Fools')
--- scripts/resources.py
+++ scripts/resources.py
@@ -1,6 +1,38 @@
 import os
 
+class LibCard:
+    def __init__(self):
+        self.cardName: str = "Unspecified"
+        self.originalName: str = ""
+        self.buffName: str = ""
+        self.spellURL: str = "None"
+        self.imgURL: str = "None"
+        self.school: str = "None"
+        self.regularPipCost: int = 0
+        self.schoolPipCost: int = 0
+        self.shadowPipCost: int = 0
+
+        self.type: list = []
+        self.desc: str = ""
+
+        self.baseAccuracy: int = 0
+        self.baseDamage: int = 0
+        self.baseSelf: int = 0
+        self.baseDOT: int = 0
+        self.baseHOT: int = 0
+        self.rounds: int = 0
+        self.basePercent: int = 0
+
+        self.buffAccuracy: int = 0
+        self.buffPierce: int = 0
+        self.buffDamage: int = 0
+        self.buffHeal: int = 0
+        self.buffPercent: int = 0
+        self.buffProtect: bool = False
+
+
 INSTALLDIR = os.path.abspath('wizmatic').replace('\wizmatic\\','\\')
 WIKIURL = 'https://www.wizard101central.com'
 
 SCHOOLS = ['Fire','Ice','Storm','Balance','Myth','Death','Life','Star','Sun','Moon','Shadow']
+SPELL_TYPE_ICONS = ['Damage Spell','Healing Spell','Charm Spell','Aura Spell','Enchantment Spell','Polymorph Spell','Shadow Self Spell','Shadow Creature Spell','All Enemies Spell','Steal Spell','Ward Spell','Global Spell','Manipulation Spell','Mutate Spell']
